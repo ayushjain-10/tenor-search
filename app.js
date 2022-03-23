74b2c0ed59d1eba41c31f8e804ab8071a95eab1b
Ayush Jain <ayushshreeshreemal@gmail.com>
2022-03-23 14:06:01 -0700

    added search form

--- app.js
+++ app.js
@@ -7,12 +7,17 @@ const { engine }  = require('express-handlebars');
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 // Routes
-app.get('/', (req, res) => {
-    // set the url of the gif
-    const gifUrl = 'https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245'
-    // render the hello-gif view, passing the gifUrl into the view to be displayed
-    res.render('hello-gif', { gifUrl })
-  })
+// example URL "http://localhost:3000/?term=hey"
+app.get('/', 
+  (req, res) => {
+    let term = "";
+    if (req.query.term) {
+      term = req.query.term
+    }
+    console.log(term);
+    res.render('home');
+  }
+);
 // Start Server
 app.listen(3000, () => {
   console.log('Gif Search listening on port localhost:3000!');
