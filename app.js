c826b36818c508919081095040446689d465c90b
Ayush Jain <ayushshreeshreemal@gmail.com>
2022-03-23 14:24:13 -0700

    added working gif search

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 // Require Libraries
 const express = require('express');
+// Require tenorjs near the top of the file
+const Tenor = require("tenorjs").client({
+    // Replace with your own key
+    "Key": "MDNNVL3QNGJI", // https://tenor.com/developer/keyregistration
+    "Filter": "high", // "off", "low", "medium", "high", not case sensitive
+    "Locale": "en_US", // Your locale here, case-sensitivity depends on input
+  });
 // App Setup
 const app = express();
 // Middleware
@@ -8,16 +15,22 @@ app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 // Routes
 // example URL "http://localhost:3000/?term=hey"
-app.get('/', 
-  (req, res) => {
-    let term = "";
+// Routes
+app.get('/', (req, res) => {
+    // Handle the home page when we haven't queried yet
+    term = ""
     if (req.query.term) {
-      term = req.query.term
+        term = req.query.term
     }
-    console.log(term);
-    res.render('home');
-  }
-);
+    // Tenor.search.Query("SEARCH KEYWORD HERE", "LIMIT HERE")
+    Tenor.Search.Query(term, "10")
+        .then(response => {
+            // store the gifs we get back from the search
+            const gifs = response;
+            // pass the gifs as an object into the home page
+            res.render('home', { gifs })
+        }).catch(console.error);
+  })
 // Start Server
 app.listen(3000, () => {
   console.log('Gif Search listening on port localhost:3000!');
